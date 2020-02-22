4a863b5986be3b8cc3f249ccd95e020e5edf383a
Artur <artur.anissimov@gmail.com>
2020-02-22 19:43:44 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Artur Anissimov on 22.02.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
