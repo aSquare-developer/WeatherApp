37786b9a568d8a638538fca3c93f7729fc4e1778
Artur <artur.anissimov@gmail.com>
2020-02-22 20:23:40 +0200

    Current Weather struct Weather Icon Manager

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,19 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Artur Anissimov on 22.02.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import UIKit
+
+struct CurrentWeather {
+    
+    let temperature: Double
+    let appearentTemperature: Double
+    let humidity: Double
+    let pressure: Double
+    let icon: UIImage
+    
+}
