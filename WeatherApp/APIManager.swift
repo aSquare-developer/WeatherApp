4e3f0c23b5727a0fedf62a73e81bc3fe774c05b9
Artur <artur.anissimov@gmail.com>
2020-02-23 20:17:27 +0200

    Default Implementation Part 1

--- WeatherApp/APIManager.swift
+++ WeatherApp/APIManager.swift
@@ -26,3 +26,48 @@ protocol APIManager {
     
     init(sessionConfiguration: URLSessionConfiguration)
 }
+
+
+extension APIManager {
+    
+    func JSONTaskWith(request: URLRequest, completionHandler: @escaping JSONCompletionHandler)  -> JSONTask {
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                let error = NSError(domain: SWINetworkingErrorDomain, code: 100, userInfo: userInfo)
+                
+                completionHandler(nil, nil, error)
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completionHandler(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                    case 200:
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: AnyObject]
+                            completionHandler(json, HTTPResponse, nil)
+                        } catch let error as NSError {
+                            completionHandler(nil, HTTPResponse, error)
+                        }
+                    default:
+                        print("We have got response status \(HTTPResponse.statusCode)")
+                }
+            }
+        }
+        return dataTask
+    }
+    
+    
+    func fetch<T>(request: URLRequest, parse: ([String: AnyObject]) -> T?, completionHandler: (APIResult<T>) -> Void) {
+        
+    }
+}
---
+++ WeatherApp/ErrorManager.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorManager.swift
+//  WeatherApp
+//
+//  Created by Artur Anissimov on 23.02.2020.
+//  Copyright © 2020 Artur Anissimov. All rights reserved.
+//
+
+import Foundation
+
+public let SWINetworkingErrorDomain = "ru.swiftbook.WeatherApp.NetworkingError"
+public let MissingHTTPResponseError = 100
