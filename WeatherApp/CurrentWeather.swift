14d6b7e519327eb0b05136d22dc957c0a1c65fb7
Artur <artur.anissimov@gmail.com>
2020-02-23 14:55:14 +0200

    Fake data try

--- WeatherApp/CurrentWeather.swift
+++ WeatherApp/CurrentWeather.swift
@@ -17,3 +17,22 @@ struct CurrentWeather {
     let icon: UIImage
     
 }
+
+extension CurrentWeather {
+    
+    var pressureString: String {
+        return "\(Int(pressure)) mm"
+    }
+    
+    var humidityString: String {
+        return "\(Int(humidity))%"
+    }
+    
+    var temperatureString: String {
+        return "\(Int(temperature))˚C"
+    }
+    
+    var appearentTemperatureString: String {
+        return "\(Int(appearentTemperature))˚C"
+    }
+}
